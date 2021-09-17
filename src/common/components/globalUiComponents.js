9fa20affdb7bec933577b35096b8f253f562a578
Dicki Maulana <bukandicki@gmail.com>
2021-09-17 10:19:50 +0700

    [ New UI ] Stepper (#13)
    * feat: Install pug
    
    * feat: Add new stepper ui component

--- src/common/components/globalUiComponents.js
+++ src/common/components/globalUiComponents.js
@@ -3,6 +3,7 @@ import Vue from "vue"
 Vue.component("ui-debio-card", () => import("@/common/components/Card"))
 Vue.component("ui-debio-avatar", () => import("@/common/components/Avatar"))
 Vue.component("ui-debio-icon", () => import("@/common/components/Icon"))
+Vue.component("ui-debio-stepper", () => import("@/common/components/Stepper"))
 
 // Forms
 Vue.component("ui-debio-input", () => import("@/common/components/Input"))
