ff03f2b78ec1b1e3d19a5b017f51b45a6b039763
luthfiazhari <luthfi@blocksphere.id>
2021-09-22 11:40:32 +0700

    Feat: Revamp FrontEnd : Table

---
+++ src/common/components/DataTable/index.vue
@@ -0,0 +1,212 @@
+<template lang="pug">
+  v-card(class="degenics-datatable-card elevation-2 ma-15")
+    slot(name="prepend")
+
+    //- <!-- Data Table -->
+    v-data-table(
+      class="degenics-data-table"
+      :headers="headers"
+      :items="items"
+      :items-per-page="5"
+      :search="dataTableSearchVal"
+      :page.sync="page"
+      :class="additionalClass"
+      hide-default-footer
+      :custom-filter="customFilter ? customFilter : defaultFilter"
+      :sort-by="sortBy"
+      :sort-desc="sortDesc"
+      :loading="loading"
+      :show-expand="expand"
+      item-key="id"
+      @click:row="clickedRow"
+      :loading-text="computedLoadingText"
+      :disable-sort="disableSort"
+    )
+      //- <!-- Slots for row formatting -->
+      template(v-for="(slot, name) in $scopedSlots" v-slot:[name]="item")
+        slot(:name="name" v-bind="item")
+
+
+    //- <!-- Custom Footer -->
+    template(v-slot:footer)
+      div(class="footer d-flex justify-space-between align-center flex-wrap")
+        div(
+          class="pagination-info"
+        ) Showing {{ from() }} to {{ to() }} of {{ total() }} entries
+        v-pagination(
+          v-model="page"
+          :length="pageCount"
+          :total-visible="10"
+          circle
+          color="secondary"
+        )
+
+</template>
+
+<script>
+export default {
+  name: 'DataTable',
+  props: {
+    headers: { type: [Object, Array]},
+    items: { type: [Object, Array]},
+    additionalClass: { type: String},
+    sortBy: { type: Array},
+    sortDesc: { type: Array},
+    loading: { type: Boolean},
+    expand: { type: Boolean},
+    customFilter: {type: [String, Boolean], default: false},
+    loadingText: {type: String, default: ""},
+    disableSort: {type: Boolean, default: false}
+  },
+  data: () => ({
+    page: 1,
+    pageCount: 0,
+    // expanded: [],
+    entriesPerPage: 10,
+    
+  }),
+  mounted() {
+  },
+  methods: {
+    clickedRow() {
+      console.log('clicked')
+    },
+    computedLoadingText() {
+      return this.loadingText ? this.loadingText : 'Loading.. Please wait'
+    },
+    defaultFilter(value, search, /*item*/) {
+      return value != null &&
+      search != null &&
+      typeof value !== 'boolean' &&
+      value.toString().toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) !== -1
+    },
+
+    /**
+     * Footer Helper functions
+     */
+    from() {
+      if (this.totalItemLength == 0) {
+        return 0
+      }
+      return this.entriesPerPage * (this.page - 1) + 1
+    },
+
+    to() {
+      if (this.page * this.entriesPerPage > this.items.length) {
+        return this.items.length
+      }
+      return this.page * this.entriesPerPage
+    },
+
+    total() {
+      return this.totalItemLength
+    },
+    /** ----------------- */
+  },
+  computed: {
+    dataTableSearchVal: {
+      get() {
+        return this.search
+      },
+      set(val) {
+        console.log(val)
+        this.$emit('input', val)
+      }
+    },
+
+  }
+}
+</script>
+
+<style lang="scss">
+// @import '@/styles/variables.scss';
+@import '/src/styles/variables.scss';
+
+.degenics-datatable-card {
+  border-radius: 8px !important;
+  overflow: hidden !important;
+  padding: 40px;
+
+  box-shadow: unset !important;
+  filter: drop-shadow(0px 7px 20px rgba(0, 0, 0, 0.07));
+}
+
+.degenics-data-table {
+  border-radius: 8px;
+  overflow: hidden;
+  margin-top: 24px;
+
+  /** BRI text-body */
+  font-size: 9px;
+  line-height: 18px;
+  letter-spacing: 0.1px;
+  /** */
+
+  thead {
+    background-color: #f5f7f9 !important;
+
+    th {
+      padding-top: 8px !important;
+      padding-bottom: 8px !important;
+      height: 29px !important;
+      /* white-space: nowrap; */
+      * {
+        color: black !important;
+        /** BRI text-body */
+        font-size: 9px;
+        line-height: 18px;
+        letter-spacing: 0.1px;
+        /** */
+      }
+      i.v-icon.notranslate.v-data-table-header__icon:not(:hover) {
+        opacity: 0.5;
+      }
+      &.active {
+        i.v-icon.notranslate.v-data-table-header__icon {
+          opacity: 1;
+        }
+      }
+    }
+  }
+
+  tbody {
+    border-bottom: none !important;
+    td {
+      * {
+        height: 57.3px !important;
+      }
+    }
+
+    tr {
+      * {
+        font-size: 9px !important;
+      }
+    }
+  }
+
+  .footer {
+    height: 72px;
+
+    .pagination-info {
+      color: $color-text-secondary;
+    }
+    .v-pagination {
+      button {
+        font-size: 11px;
+        min-width: 24px;
+        height: 24px;
+        outline: none;
+        /* outline-color: $color-secondary; */
+        &:not(.v-pagination__item--active) {
+          background: $color-border;
+        }
+      }
+      li:first-child, li:last-child {
+        button {
+          width: 24px;
+        }
+      }
+    }
+  }
+}
+</style>
