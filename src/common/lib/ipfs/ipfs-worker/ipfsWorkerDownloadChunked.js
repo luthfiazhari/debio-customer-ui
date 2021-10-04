f230011ed13e6bba82b4ce95ab9439cc9203ab58
Dicki Maulana <bukandicki@gmail.com>
2021-10-04 17:45:02 +0700

    [ FIX ] New Linter Errors (#29)
    * feat: Install pug
    
    * fix: Linter errors

--- src/common/lib/ipfs/ipfs-worker/ipfsWorkerDownloadChunked.js
+++ src/common/lib/ipfs/ipfs-worker/ipfsWorkerDownloadChunked.js
@@ -1,30 +1,29 @@
-/* eslint-disable */
-import IPFSHttpClient from 'ipfs-http-client';
-import Kilt from '@kiltprotocol/sdk-js';
+import IPFSHttpClient from "ipfs-http-client"
+import Kilt from "@kiltprotocol/sdk-js"
 
 onmessage = function (e) {
-  console.log("Downloading...");
+  console.log("Downloading...")
 
-  const ipfs = IPFSHttpClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+  const ipfs = IPFSHttpClient({ host: "ipfs.infura.io", port: 5001, protocol: "https" });
   (async (ipfs) => {
-    let dts;
-    const res = await ipfs.get("/ipfs/" + e.data.path);
-    const content = await res.next();
-    let dt = await content.value.content.next();
+    let dts
+    const res = await ipfs.get("/ipfs/" + e.data.path)
+    const content = await res.next()
+    let dt = await content.value.content.next()
     if (!dts) {
-      dts = dt.value;
+      dts = dt.value
     }
     for await (let dtd of content.value.content) {
-      dts = constructFile(dts, dtd);
+      dts = constructFile(dts, dtd)
     }
 
-    console.log('dts is -> ', dts)
-    const fl = new Blob([dts], { type: e.data.typeFile });
-    let encrypted = await fl.text();
+    console.log("dts is -> ", dts)
+    const fl = new Blob([dts], { type: e.data.typeFile })
+    let encrypted = await fl.text()
     encrypted = JSON.parse(encrypted)
     encrypted = encrypted[0]
 
-    console.log('encrypted', encrypted)
+    console.log("encrypted", encrypted)
     let { box, nonce } = encrypted.data
     box = Object.values(box) // Convert from object to Array
     nonce = Object.values(nonce) // Convert from object to Array
@@ -34,29 +33,29 @@ onmessage = function (e) {
       nonce: Uint8Array.from(nonce)
     }
 
-    if (e.data.typeFile == 'application/pdf') {
+    if (e.data.typeFile == "application/pdf") {
       const decryptedObject = await Kilt.Utils.Crypto.decryptAsymmetric(toDecrypt, e.data.pair.publicKey, e.data.pair.secretKey)
       return decryptedObject
     }
 
-    let objs = "";
-    const decryptedObject = await Kilt.Utils.Crypto.decryptAsymmetricAsStr(toDecrypt, e.data.pair.publicKey, e.data.pair.secretKey);
+    let objs = ""
+    const decryptedObject = await Kilt.Utils.Crypto.decryptAsymmetricAsStr(toDecrypt, e.data.pair.publicKey, e.data.pair.secretKey)
     if (!decryptedObject) {
-      console.log('undefined', decryptedObject);
+      console.log("undefined", decryptedObject)
     }
     if (decryptedObject) {
-      objs += decryptedObject;
+      objs += decryptedObject
     }
-    dts = null;
-    return objs;
+    dts = null
+    return objs
   })(ipfs).then(res => {
-    postMessage(res);
-  });
+    postMessage(res)
+  })
 }
 
 function constructFile(chunk1, chunk2) {
-  var tmp = new Uint8Array(chunk1.length + chunk2.length);
-  tmp.set(chunk1);
-  tmp.set(chunk2, chunk1.length);
-  return tmp;
+  var tmp = new Uint8Array(chunk1.length + chunk2.length)
+  tmp.set(chunk1)
+  tmp.set(chunk2, chunk1.length)
+  return tmp
 }
