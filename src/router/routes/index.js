d74845ae03aaa40e4226f78fea6a1255220482dc
ciniiia <87380683+ciniiia@users.noreply.github.com>
2021-11-30 17:35:11 +0800

    fix: customer routes (#158)
    Co-authored-by: ciniiia <cinia@blocksphere.com>

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -2,13 +2,20 @@ import { checkIsLoggedIn } from "@/common/lib/route-guard"
 
 const indexRoutes = [{
   path: "/",
+  name: "dashboard",
+  beforeEnter: checkIsLoggedIn,
+  redirect: { name: "customer-dashboard" },
+  component: () => import(/* webpackChunkName */ "@/views/Dashboard/Customer/Layout")
+},
+{
+  path: "/customer",
   component: () => import(/* webpackChunkName */ "@/views/Dashboard/Customer/Layout"),
   beforeEnter: checkIsLoggedIn,
   name: "customer",
   redirect: { name: "customer-dashboard" },
   children: [
     {
-      path: "/customer",
+      path: "/",
       name: "customer-dashboard",
       meta: { pageHeader: "Home" },
       component: () => import(/* webpackChunkName */ "@/views/Dashboard/Customer/Home")
