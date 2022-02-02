3b48aeeff0dd39ac329d44d40f75c49c4bb1e530
luthfiazhari <luthfi@blocksphere.id>
2022-02-02 09:47:22 +0700

    Tests: revise unit testing input component

--- tests/unit/common/components/input.spec.js
+++ tests/unit/common/components/input.spec.js
@@ -1,6 +1,5 @@
 import { mount } from "@vue/test-utils"
 import Input from "@/common/components/Input"
-// import Button from "@/common/components/Button"
 import Vue from "vue"
 import Vuetify from  "vuetify"
 
