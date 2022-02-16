d0f49b56ea5f0357cdb03ec6ae59660709c0d782
luthfiazhari <luthfi@blocksphere.id>
2022-02-16 09:15:24 +0700

    Fetch Feat: Genetic Analysis list tab

--- src/views/Dashboard/Customer/Home/GeneticData/GeneticAnalysisList.vue
+++ src/views/Dashboard/Customer/Home/GeneticData/GeneticAnalysisList.vue
@@ -4,6 +4,25 @@
       :headers="headers"
       :items="items"
     )
+      template(v-slot:[`item.serviceName`]="{ item }")
+        .d-flex.flex-column.genetic-analysis-list__service
+          span {{ item.serviceName }}
+      
+      template(v-slot:[`item.fullName`]="{ item }")
+        .d-flex.flex-column.genetic-analysis-list__name
+          span {{ item.fullName }}
+
+      
+
+      template(v-slot:[`item.status`]="{ item }")
+        .d-flex.flex-column.genetic-analysis-list__status
+          span {{ item.status }}
+
+    
+      template(v-slot:[`item.actions`]="{ item }")
+        .genetic-analysis-list__actions
+          ui-debio-icon(v-if="!iconShow" :icon="eyeIcon" size="16" role="button" stroke @click="toDetail()")
+          ui-debio-icon(v-if="iconDownloadShow" :icon="downloadIcon" size="16" role="button" stroke @click="toDownload()")
 
 </template>
 
@@ -14,7 +33,6 @@ import { eyeIcon, downloadIcon } from "@/common/icons"
 import DataTable from "@/common/components/DataTable"
 import { geneticAnalysisByOwner, geneticAnalysisStorage, geneticAnalysisOrders } from "@/common/lib/polkadot-provider/query/genetic-analysis"
 import { geneticAnalysts, geneticAnalystServices } from "@/common/lib/polkadot-provider/query/genetic-analyst"
-// import { queryGeneticDataByOwner, queryGeneticDataById } from "@/common/lib/polkadot-provider/query/genetic-data"
 
 export default {
   name: "GeneticAnalysisList",
@@ -67,72 +85,104 @@ export default {
         sortable: false
       }
     ],
-    items: []
+    items: [],
+    iconShow: true,
+    iconDownloadShow: true
   }),
 
   async mounted() {
     this.fetchGeneticAnalysisData()
+    this.checkStatusData()
   },
 
   methods: {
     async fetchGeneticAnalysisData() {
       this.items = []
-      // const accountId = this.wallet.address
-      const accountId = "5GxL2PAEqGGnJNCsiYBv8qSZKNRcd8mn7tuDj1Hd73qe23eF"
+      const accountId = this.wallet.address
       const trackingId = await geneticAnalysisByOwner(this.api, accountId)
-      // console.log(accountId, "account")
-      // console.log(trackingId, "data items")
 
       for (let i = 0; i < trackingId.length; i++) {
-        console.log(trackingId[i], [i], "tracking ID")
         const geneticAnalysis = await geneticAnalysisStorage(this.api, trackingId[i])
-        console.log("abis looping", [i])
-        console.log(geneticAnalysis, "genetic storage")
 
-        const updatedAt = geneticAnalysis.updatedAt.toLocaleString("en-GB", { 
+        const dateCreated = new Date(parseInt(geneticAnalysis.createdAt.replace(/,/g, "")))
+        const dateUpdated = new Date(parseInt(geneticAnalysis.updatedAt.replace(/,/g, "")))
+
+        const updatedAt = dateUpdated.toLocaleString("en-GB", { 
           day: "numeric", // numeric, 2-digit
           year: "numeric", // numeric, 2-digit
           month: "short" // numeric, 2-digit, long, short, narrow
         })
 
-        const createdAt = geneticAnalysis.createdAt.toLocaleString("en-GB", {
+        const createdAt = dateCreated.toLocaleString("en-GB", {
           day: "numeric", // numeric, 2-digit
           year: "numeric", // numeric, 2-digit
           month: "short" // numeric, 2-digit, long, short, narrow
         })
 
         const geneticAnalysisTrackingId = geneticAnalysis.geneticAnalystId
-        console.log(geneticAnalysisTrackingId, "geneticAnalysisTrackingId")
-
 
         const geneticAnalystsData = await geneticAnalysts(this.api, geneticAnalysisTrackingId)
-        console.log(geneticAnalystsData, "geneticAnalystData")
-        const fullName = (geneticAnalystsData.info.firstName, " ", geneticAnalystsData.info.lastName)
-
+        const fullName = (geneticAnalystsData.info.firstName + " " + geneticAnalystsData.info.lastName)
 
         const orderId = geneticAnalysis.geneticAnalysisOrderId
         const geneticAnalysisOrdersData = await geneticAnalysisOrders(this.api, orderId)
-        console.log(geneticAnalysisOrdersData, "geneticAnalysisOrderData")
-
 
         const serviceId = geneticAnalysisOrdersData.serviceId
         const geneticAnalystServicesData = await geneticAnalystServices(this.api, serviceId)
-        console.log(geneticAnalystServicesData, "geneticAnalystsServiceData")
-
 
         const dataResult = {
+          trackingId: trackingId[i],
           serviceName: geneticAnalystServicesData.info.name,
           analystName: fullName,
           createdAt: createdAt,
-          updatedAt: updatedAt
+          updatedAt: updatedAt,
+          status: geneticAnalysis.status
         }
         this.items.push(dataResult)
+      }
+    },
+
+    toDetail() {
+      // console.log(geneticId)
+      // Open new tab with sending props geneticId for fetch detail
+    },
+
+    toDownload(){
+      //Do something
+    },
+
+    checkStatusData() {
+      for(let i = 0; i < this.items.length; i++) {
+        if(this.items.length.status == "ResultReady") {
+          this.iconDownloadShow = false
+        }
       }
     }
   }
 }
 </script>
 
-<style>
+<style lang="sass" scoped>
+  @import "@/common/styles/mixins.sass"
 
+  .genetic-analysis-list
+    padding: 0 !important
+    margin: -50px -28px 0 -28px
+
+
+    &__service
+      width: 117px
+    
+    &__fullName
+      width: 480px
+
+    &__status
+      width: 65px
+
+    &__actions
+      width: 45px
+      display: flex
+      align-items: center
+      justify-content: center
+      gap: 20px
 </style>
