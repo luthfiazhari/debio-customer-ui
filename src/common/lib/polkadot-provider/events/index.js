14511d1ccb784019e02210df3ffb8683d0222142
Azmi Muhammad <31072582+azmimuhammad@users.noreply.github.com>
2022-02-17 20:45:04 +0700

    [DBIO-1774] fix: notification unstake (#304)
    * fix: handle events index condition
    
    * fix: condition events index value
    
    Co-authored-by: Hildegard Lydia <37687466+hilyds@users.noreply.github.com>

--- src/common/lib/polkadot-provider/events/index.js
+++ src/common/lib/polkadot-provider/events/index.js
@@ -27,7 +27,7 @@ export async function processEvent(state, address, event, role) {
 
     const res = await handler(dataEvent, value, valueMessage)
     
-    if (res.data[identity] || res.data[value][identity] == address) {
+    if (res.data[identity] || res.data[1][identity] == address) {
       statusAdd = true
       message = state.configEvent["role"][role][event.section][event.method].message + " " + res.wording
     }
